ddae47c62fe60261ccfe69512a0633ce6dce50cf
Daniel Monaghan <mowglixx@users.noreply.github.com>
2021-12-16 16:37:15 +0000

    added features and variables

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+PORTAINER_VERSION=2.9.3
+DOCKER_COMPOSE_VERSION=2.2.2
+
 if [ $(whoami) != 'root' ]; then
   echo "Please run as root"
 
@@ -29,5 +32,13 @@ apt update
 
 # install docker
 apt install docker-ce docker-ce-cli containerd.io docker-compose
-curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# run portainer with no ports exposed
+docker volume create portainer_data
+docker run -d --expose 8000 --expose 9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data cr.portainer.io/portainer/portainer-ce:$PORTAINER_VERSION
+
+# run nginx proxy manager
+docker volume create nginx_data
+docker run -d -p 80:80 -p 81:81 -p 443:443 -p 8080:8080 -p 8443:8443 --name nginx-proxy-manager --restart=always -v nginx_data:/data -v /etc/letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
